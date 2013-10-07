bca0e6d5ad66c41b38ca59300a5df1b03bd6dae0
Robert Giesecke <robert.giesecke@gmail.com>
2013-10-07 18:29:38 +0200

    added the class VertexBuilder that can build vertex graphs from a source list and a dependencySelector

---
+++ StronglyConnectedComponents/Core/VertexBuilder.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace StronglyConnectedComponents.Core
+{
+	public static class VertexBuilder
+	{
+		public static IEnumerable<Vertex<T>> BuildVertices<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> dependencySelector, IEqualityComparer<T> comparer)
+		{
+			if (source == null) throw new ArgumentNullException("source");
+			if (dependencySelector == null) throw new ArgumentNullException("getDependencies");
+			if (comparer == null) throw new ArgumentNullException("comparer");
+
+			var vertexBySource = new ConcurrentDictionary<T, Vertex<T>>(comparer);
+
+			return source.Select(t => BuildVertex(vertexBySource, t, dependencySelector));
+		}
+
+    private static Vertex<T> BuildVertex<T>(ConcurrentDictionary<T, Vertex<T>> vertexBySource, T source, Func<T, IEnumerable<T>> dependencySelector)
+		{
+			Vertex<T> vertex;
+			if (vertexBySource.TryGetValue(source, out vertex))
+				return vertex;
+
+			if (vertexBySource.TryAdd(source, vertex = new Vertex<T>(source)))
+			{
+				foreach (var d in dependencySelector(vertex.Value))
+					vertex.Dependencies.Add(BuildVertex(vertexBySource, d, dependencySelector));
+				return vertex;
+			}
+
+			return vertexBySource[source];
+		}
+	}
+}
