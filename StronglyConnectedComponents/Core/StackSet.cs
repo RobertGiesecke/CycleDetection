7a836534406f63e0284d25d23183ed8d6b2eb445
Robert Giesecke <robert.giesecke@gmail.com>
2013-10-08 10:43:17 +0200

    - changed tabs to spaces

--- StronglyConnectedComponents/Core/StackSet.cs
+++ StronglyConnectedComponents/Core/StackSet.cs
@@ -4,96 +4,96 @@ using System.Collections.Generic;
 
 namespace StronglyConnectedComponents.Core
 {
-	/// <summary>
-	/// A wrapper around a Stack<T> which uses a <see cref="IEqualityComparer{T}"/> and a <see cref="HashSet{T}"/> to make value identity configurable.
-	/// </summary>
-	internal class StackSet<T> : ICollection<T>
-	{
-		private readonly HashSet<T> _Set;
-		private readonly Stack<T> _Stack;
+  /// <summary>
+  /// A wrapper around a Stack<T> which uses a <see cref="IEqualityComparer{T}"/> and a <see cref="HashSet{T}"/> to make value identity configurable.
+  /// </summary>
+  internal class StackSet<T> : ICollection<T>
+  {
+    private readonly HashSet<T> _Set;
+    private readonly Stack<T> _Stack;
 
-		public StackSet(IEqualityComparer<T> comparer = null)
-		{
-			_Stack = new Stack<T>();
-			_Set = comparer != null ? new HashSet<T>(comparer) : new HashSet<T>();
-		}
+    public StackSet(IEqualityComparer<T> comparer = null)
+    {
+      _Stack = new Stack<T>();
+      _Set = comparer != null ? new HashSet<T>(comparer) : new HashSet<T>();
+    }
 
-		public IEqualityComparer<T> Comparer
-		{
-			get { return _Set.Comparer; }
-		}
+    public IEqualityComparer<T> Comparer
+    {
+      get { return _Set.Comparer; }
+    }
 
-		void ICollection<T>.Add(T item)
-		{
-			_Stack.Push(item);
-		}
+    void ICollection<T>.Add(T item)
+    {
+      _Stack.Push(item);
+    }
 
-		public void Clear()
-		{
-			_Stack.Clear();
-		}
+    public void Clear()
+    {
+      _Stack.Clear();
+    }
 
-		public bool Contains(T item)
-		{
-			return _Set.Contains(item);
-		}
+    public bool Contains(T item)
+    {
+      return _Set.Contains(item);
+    }
 
 
-		void ICollection<T>.CopyTo(T[] array, int arrayIndex)
-		{
-			_Stack.CopyTo(array, arrayIndex);
-		}
+    void ICollection<T>.CopyTo(T[] array, int arrayIndex)
+    {
+      _Stack.CopyTo(array, arrayIndex);
+    }
 
-		bool ICollection<T>.Remove(T item)
-		{
-			throw new NotImplementedException();
-		}
+    bool ICollection<T>.Remove(T item)
+    {
+      throw new NotImplementedException();
+    }
 
-		public int Count
-		{
-			get { return _Stack.Count; }
-		}
+    public int Count
+    {
+      get { return _Stack.Count; }
+    }
 
-		bool ICollection<T>.IsReadOnly { get { return false; } }
+    bool ICollection<T>.IsReadOnly { get { return false; } }
 
-		IEnumerator IEnumerable.GetEnumerator()
-		{
-			return GetEnumerator();
-		}
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
 
-		public IEnumerator<T> GetEnumerator()
-		{
-			return _Stack.GetEnumerator();
-		}
+    public IEnumerator<T> GetEnumerator()
+    {
+      return _Stack.GetEnumerator();
+    }
 
-		public T Peek()
-		{
-			return _Stack.Peek();
-		}
+    public T Peek()
+    {
+      return _Stack.Peek();
+    }
 
-		public T Pop()
-		{
-			var popped = _Stack.Pop();
-			_Set.Remove(popped);
-			return popped;
-		}
+    public T Pop()
+    {
+      var popped = _Stack.Pop();
+      _Set.Remove(popped);
+      return popped;
+    }
 
-		public void Push(T item)
-		{
-			if (!_Set.Add(item))
-				throw new ArgumentException("An identical item has already been pushed.", "item");
-			_Stack.Push(item);
-		}
+    public void Push(T item)
+    {
+      if (!_Set.Add(item))
+        throw new ArgumentException("An identical item has already been pushed.", "item");
+      _Stack.Push(item);
+    }
 
-		public T[] ToArray()
-		{
-			return _Stack.ToArray();
-		}
+    public T[] ToArray()
+    {
+      return _Stack.ToArray();
+    }
 
-		public void TrimExcess()
-		{
-			_Stack.TrimExcess();
-			_Set.TrimExcess();
-		}
-	}
+    public void TrimExcess()
+    {
+      _Stack.TrimExcess();
+      _Set.TrimExcess();
+    }
+  }
 }
--- StronglyConnectedComponents/Core/VertexBuilder.cs
+++ StronglyConnectedComponents/Core/VertexBuilder.cs
@@ -5,36 +5,36 @@ using System.Linq;
 
 namespace StronglyConnectedComponents.Core
 {
-	public static class VertexBuilder
-	{
-		public static IEnumerable<Vertex<T>> BuildVertices<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> dependencySelector, IEqualityComparer<T> comparer)
-		{
-			if (source == null) throw new ArgumentNullException("source");
-			if (dependencySelector == null) throw new ArgumentNullException("getDependencies");
-			if (comparer == null) throw new ArgumentNullException("comparer");
+  public static class VertexBuilder
+  {
+    public static IEnumerable<Vertex<T>> BuildVertices<T>(this IEnumerable<T> source, Func<T, IEnumerable<T>> dependencySelector, IEqualityComparer<T> comparer)
+    {
+      if (source == null) throw new ArgumentNullException("source");
+      if (dependencySelector == null) throw new ArgumentNullException("getDependencies");
+      if (comparer == null) throw new ArgumentNullException("comparer");
 
-			var vertexBySource = new ConcurrentDictionary<T, Vertex<T>>(comparer);
+      var vertexBySource = new ConcurrentDictionary<T, Vertex<T>>(comparer);
 
-			return source.Select(t => BuildVertex(vertexBySource, t, dependencySelector));
-		}
+      return source.Select(t => BuildVertex(vertexBySource, t, dependencySelector));
+    }
 
     private static Vertex<T> BuildVertex<T>(ConcurrentDictionary<T, Vertex<T>> vertexBySource, T source, Func<T, IEnumerable<T>> dependencySelector)
-		{
-			Vertex<T> vertex;
-			if (vertexBySource.TryGetValue(source, out vertex))
-				return vertex;
+    {
+      Vertex<T> vertex;
+      if (vertexBySource.TryGetValue(source, out vertex))
+        return vertex;
 
-			if (vertexBySource.TryAdd(source, vertex = new Vertex<T>(source)))
-			{
-				var dependencies = dependencySelector(vertex.Value);
+      if (vertexBySource.TryAdd(source, vertex = new Vertex<T>(source)))
+      {
+        var dependencies = dependencySelector(vertex.Value);
 
-				if (dependencies != null)
-					foreach (var d in dependencies)
-						vertex.Dependencies.Add(BuildVertex(vertexBySource, d, dependencySelector));
-				return vertex;
-			}
+        if (dependencies != null)
+          foreach (var d in dependencies)
+            vertex.Dependencies.Add(BuildVertex(vertexBySource, d, dependencySelector));
+        return vertex;
+      }
 
-			return vertexBySource[source];
-		}
-	}
+      return vertexBySource[source];
+    }
+  }
 }
